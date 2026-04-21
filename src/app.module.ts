2861fdb187205466b7b9ca9077f56934c3722ca4
Mike Mahan (ext) <michaell.mahan.contractor@bbva.com>
2026-04-21 11:53:04 +0200

    add modules important

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ClientsModule } from './clients/clients.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { InvoicesModule } from './invoices/invoices.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ClientsModule,
+    ProductsModule,
+    OrdersModule,
+    InvoicesModule,
+  ],
   controllers: [],
   providers: [],
 })
