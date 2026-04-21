46dc3855e2712897c21c362ce70ccb101dc14cd9
Mike Mahan (ext) <michaell.mahan.contractor@bbva.com>
2026-04-21 13:56:48 +0200

    install config servier and typoeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,29 @@ import { ClientsModule } from './clients/clients.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { InvoicesModule } from './invoices/invoices.module';
+import { TaxModule } from './tax/tax.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.BD_HOST,
+      port: process.env.BD_PORT ? parseInt(process.env.BD_PORT) : 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, //carga las entidades en la bd que se crean
+      synchronize: true, //Solo en dev en pro no se usa.
+    }),
     UsersModule,
     ClientsModule,
     ProductsModule,
     OrdersModule,
     InvoicesModule,
+    TaxModule,
   ],
   controllers: [],
   providers: [],
